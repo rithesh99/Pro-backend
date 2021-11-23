440f7bfec1b39d0ed447ef8556f5a1e87324c340
rithesh99 <ritheshdavid@gmail.com>
2021-11-23 10:08:59 +0530

    Is logged in middleware

---
+++ 06_ecommerce/middlewares/user.js
@@ -0,0 +1,15 @@
+const BigPromise = require('../middlewares/bigPromise')
+const User = require('../models/User')
+const CustomError = require('../utils/customError')
+const jwt = require('jsonwebtoken')
+
+exports.isLoggedIn = BigPromise(async (req, res) => {
+    const token = req.cookies.token || req.header('Authorization').replace("Bearer ", "")
+    if (!token) {
+        return next(new CustomError("Login first to access this page", 401))
+    }
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    //my property - middleware
+    req.user = awaitUser.findById(decoded.id)
+    next();
+})
