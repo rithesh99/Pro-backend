e809e736e0b268e014df248fc61b060fd2a7125c
rithesh99 <ritheshdavid@gmail.com>
2021-11-27 23:14:00 +0530

    My orders

--- 06_ecommerce/routes/order.js
+++ 06_ecommerce/routes/order.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const {
     createOrder,
-    getOneOrder
+    getOneOrder,
+    getLoggedInOrders
 } = require("../controllers/orderController");
 const router = express.Router();
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
 router.route("/order/:id").get(isLoggedIn, getOneOrder);
+router.route("/myorder").get(isLoggedIn, getLoggedInOrders);
 
 module.exports = router;
