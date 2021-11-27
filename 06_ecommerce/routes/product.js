fdf15aef8cef4c8b15d9a2062100265f746ee417
rithesh99 <ritheshdavid@gmail.com>
2021-11-27 13:04:58 +0530

    Routes for reviews

--- 06_ecommerce/routes/product.js
+++ 06_ecommerce/routes/product.js
@@ -3,11 +3,15 @@ const router = express.Router()
 const { isLoggedIn, customRole } = require("../middlewares/user")
 
 const { addProduct, getAllProducts, adminGetAllProducts,
-    getOneProduct, adminUpdateONeProduct, adminDeleteOneProduct } = require('../controllers/productController')
+    getOneProduct, adminUpdateONeProduct, adminDeleteOneProduct,
+    addReview, deleteReview, getOnlyReviewsForOneProduct } = require('../controllers/productController')
 
 //user routes
 router.route('/products').get(getAllProducts)
 router.route('/product/:id').get(getOneProduct)
+router.route("/review").put(isLoggedIn, addReview);
+router.route("/review").delete(isLoggedIn, deleteReview);
+router.route("/reviews").get(isLoggedIn, getOnlyReviewsForOneProduct);
 
 //admin routes  
 router.route('/admin/product/add').post(isLoggedIn, customRole("admin"), addProduct)
