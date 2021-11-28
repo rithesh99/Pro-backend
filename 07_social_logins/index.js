468d4dbaaf9e02d8f460fe4c6a8ed27a88d929f5
rithesh99 <ritheshdavid@gmail.com>
2021-11-28 20:40:57 +0530

    DB connection and home, login pages

--- 07_social_logins/index.js
+++ 07_social_logins/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const auth = require('./routes/auth')
 const app = express();
 
+//connect to DB
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
+    console.log("DB CONNECTED")
+);
+
+app.set('view engine', 'ejs')
+app.use('/auth', auth)
+
 app.get("/", (req, res) => {
-    return res.json({
-        success: true,
-        message: "home route"
-    })
+    return res.render('home')
 })
 
 
